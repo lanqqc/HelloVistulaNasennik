b4ce1456b3c5017979131b6a7e507c388cf9c39c
Pavel Nasennik <115161430+lanqqc@users.noreply.github.com>
2022-10-06 15:45:39 +0200

    Add files via upload

---
+++ HelloVistulaNasennik/src/pl/vistula/helloworldnasennik/HelloWorldNasennik.java
@@ -0,0 +1,10 @@
+package pl.vistula.helloworldnasennik;
+
+public class HelloWorldNasennik {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula Nasennik");
+        System.out.println("I am 21 years old student from Belarus, now living in Warsaw,Poland.");
+        System.out.println("I like to talk with ppl ;x");
+
+    }
+}
